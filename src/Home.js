443a7d552ff9751a4a132a996552e9564b22f548
Matteo <matteo@pc-di-matteo.station>
2022-07-11 15:38:46 +0200

    2022 07 11

--- src/Home.js
+++ src/Home.js
@@ -5,53 +5,64 @@ import logo from "./imgs/dike_blk_bg.png"
 const Home = () => {
 
     // @media queries
-    const [matches, setMatches] = useState(
+    const [s_screen, setS_screen] = useState(
         window.matchMedia("(min-width: 768px)").matches
-      )
+      );
+
+      
+    useEffect(() => {
+        window
+        .matchMedia("(min-width: 768px)")
+        .addEventListener('change', e => setS_screen( e.matches ));
+    }, []);
+        
+    /*const [m_screen, setM_screen] = useState(
+            window.matchMedia("(min-width: 768px)").matches
+        );
 
     useEffect(() => {
         window
         .matchMedia("(min-width: 768px)")
-        .addEventListener('change', e => setMatches( e.matches ));
-    }, []);
-
+        .addEventListener('change', e => setS_screen( e.matches ));
+    }, []);*/
 
     const quotes = [
         {
             quote: "“Arriva il momento in cui bisogna scegliere tra ciò che è facile e ciò che è giusto.”",
             author: "Albus Percival Wulfric Brian Silente",
         },
-        {
-            quote: "“Time to conquer west Russia”",
-            author: "Putin, probably",
-        }
+        
     ]
     const idx = Math.floor(Math.random() * quotes.length);
 
     return (
         <div>
-            { matches && (<div className="home">
-                    <img className="img" src={logo} alt=''/>
-                    <div className="text debug">
-                        <p className="quote">
-                            {quotes[idx].quote}
-                        </p>
-                        <p className="author">{quotes[idx].author}</p>
+            { s_screen && (
+                    <div className="home">
+                        <img className="img" src={logo} alt=''/>
+                        <div className="text debug">
+                            <p className="quote">
+                                {quotes[idx].quote}
+                            </p>
+                            <p className="author">{quotes[idx].author}</p>
 
+                        </div>
                     </div>
-                </div>)
+                )
             }
 
-            { !matches && (<div className="s_home">
-                    <img className="s_img" src={logo} alt=''/>
-                    <div className="text debug">
-                        <p className="quote">
-                            {quotes[idx].quote}
-                        </p>
-                        <p className="author">{quotes[idx].author}</p>
+            { !s_screen && (
+                    <div className="s_home">
+                        <img className="s_img" src={logo} alt=''/>
+                        <div className="text debug">
+                            <p className="quote">
+                                {quotes[idx].quote}
+                            </p>
+                            <p className="author">{quotes[idx].author}</p>
 
+                        </div>
                     </div>
-                </div>)
+                )
             }
         </div>
     );
