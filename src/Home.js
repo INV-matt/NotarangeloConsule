cab6c2eb71cb17dc1cc8994d9d3a3686b9dc75dc
Matteo <matteo@pc-di-matteo.station>
2022-07-13 12:36:39 +0200

    latest

--- src/Home.js
+++ src/Home.js
@@ -31,10 +31,45 @@ const Home = () => {
             author: "Albus Percival Wulfric Brian Silente",
         },
         {
-            quote: "“Scegli il meglio, scegli E-CAMPUS. Ti aspecto!”",
-            author: "Cristiano “THE SIUUUUU” Ronaldo",
+            quote: "“Tra vent'ann non sarete delusi dalle cose che avete fatto, ma da quelle che non avrete fatto. Allora levate l'ancora, abbandonate i porti sicuri, catturate il vento nelle vostre vele, esplorate, sognate, scoprite.”",
+            author: "Mark Twain",
         },
-        
+        {
+            quote: "“Quando sei davanti a due decisioni, lancia in aria una moneta. Non perché farà la scelta giusta al posto tuo, ma perché nell'esatto momento in cui essa è in aria, saprai improvvisamente in cosa stai sperando di più.”",
+            author: "Bob Marley",
+        },
+        {
+            quote: "“All'inizio le battaglie, le vincono i falsi, perché sono troppo furbi. Ma la guerra la vince l'onesto, perché perservera nella pazienza e nella giustizia...”",
+            author: "L. Valentinis",
+        },
+        {
+            quote: "“Molti, dicendo cose giuste, fanno cose ingiuste.”",
+            author: "Senofonte",
+        },
+        {
+            quote: "“La pazienza, la perseveranza e il sudato lavoro creano un'imbattibile combinazione per il successo.”",
+            author: "Napoleon Hill",
+        },
+        {
+            quote: "“Non si fa il proprio dovere perché qualcuno ci dica grazie, lo si fa per principio, per se stessi, per la propria dinità.”",
+            author: "Oriana Fallaci",
+        },
+        {
+            quote: "“Sapere ciò che è giusto e non farlo è la peggiore vigliaccheria.”",
+            author: "Confucio",
+        },
+        {
+            quote: "“Se un uomo non è disponibile a correre qualche rischio per le proprie idee, o le sue idee non valgono nulla o è lui che non vale nulla.”",
+            author: "Ezra Pound",
+        },
+        {
+            quote: "“La scienza che si discosta dalla giustizia, più che scienza deve chiamarsi astuzia.”",
+            author: "Marco Tullio Cicerone",
+        },
+        /*{
+            quote: "“”",
+            author: "",
+        },*/
     ]
     const idx = Math.floor(Math.random() * quotes.length);
 
